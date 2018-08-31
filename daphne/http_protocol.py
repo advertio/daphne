293a9c13543d76ef12dba0f73e4e0544d5cbd3d5
Gonçalo Cabrita <_@gmcabrita.com>
2018-08-31 17:46:30 +0100

    dir

--- daphne/http_protocol.py
+++ daphne/http_protocol.py
@@ -260,7 +260,7 @@ class WebRequest(http.Request):
                     # The path is malformed somehow - do our best to log something
                     uri = repr(self.uri)
                 try:
-                    logger.info(vars(message))
+                    logger.info(dir(message))
                     self.server.log_action(
                         "http",
                         "complete",
